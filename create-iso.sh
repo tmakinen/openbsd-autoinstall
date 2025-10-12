c4e621a318442f7f88d5559239740a6edfadad7c
Timo Makinen <tmakinen@foo.sh>
2025-10-12 16:26:19 +0000

    Add uEFI boot support

--- create-iso.sh
+++ create-iso.sh
@@ -25,8 +25,10 @@ for f in cdboot cdbr; do
     echo "Downloading ${f}..."
     curl -sSf -o "${WORKDIR}/iso/${OSREV}/${MACHINE}/${f}" "${URL}/${f}"
 done
-echo "Downloading bsd.rd..."
-curl -sSf -o "${WORKDIR}/bsd.rd" "${URL}/bsd.rd"
+for f in BOOTIA32.EFI BOOTX64.EFI bsd.rd ; do
+    echo "Downloading ${f}..."
+    curl -sSf -o "${WORKDIR}/${f}" "${URL}/${f}"
+done
 
 echo "Creating custom bsd.rd..."
 zcat "${WORKDIR}/bsd.rd" > "${WORKDIR}/bsd.urd"
@@ -47,6 +49,13 @@ mkdir "${WORKDIR}/iso/etc"
     echo "boot"
 } > "${WORKDIR}/iso/etc/boot.conf"
 
+echo "Creating eficdboot..."
+mkdir -p "${WORKDIR}/eficdboot-dir/efi/boot"
+cp "${WORKDIR}/BOOTIA32.EFI" "${WORKDIR}/BOOTX64.EFI" \
+    "${WORKDIR}/eficdboot-dir/efi/boot/"
+makefs -t msdos -o create_size=350K \
+    "${WORKDIR}/iso/${OSREV}/${MACHINE}/eficdboot" "${WORKDIR}/eficdboot-dir"
+
 echo "Creating iso image..."
 mkhybrid -a -R -T -L -l -d -D -N -o openbsd-autoinstall.iso \
   -A "OpenBSD ${OSREV} ${MACHINE} Autoinstall CD" \
@@ -54,4 +63,5 @@ mkhybrid -a -R -T -L -l -d -D -N -o openbsd-autoinstall.iso \
   -p "Timo Maekinen <tmakinen@foo.sh>" \
   -V "OpenBSD/${MACHINE}   ${OSREV} Install CD" \
   -b "${OSREV}/${MACHINE}/cdbr" -c "${OSREV}/${MACHINE}boot.catalog" \
+  -e "${OSREV}/${MACHINE}/eficdboot" \
   "${WORKDIR}/iso"
