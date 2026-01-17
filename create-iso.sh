a24ad0c8f5fc1b62b271a1022735c253fe4f1969
Timo Makinen <tmakinen@foo.sh>
2026-01-17 17:45:12 +0000

    Split rd and iso creation to own scripts

--- create-iso.sh
+++ create-iso.sh
@@ -13,43 +13,19 @@ MACHINE="$(basename "$1")"
 OSREV="$(basename "$(dirname "$1")")"
 
 WORKDIR="$(mktemp -d)"
-trap '{
-    if [ -n "$_vnd" ]; then
-        mount | grep -Eq "^/dev/${_vnd}a on ${WORKDIR}/mount " && \
-            umount "${WORKDIR}/mount"
-        vnconfig -l "$_vnd" | grep -Fq "not in use" || \
-            vnconfig -u "$_vnd"
-    fi
-    rm -rf "${WORKDIR}"
-}' EXIT
-mkdir "${WORKDIR}/mount"
+trap 'rm -rf "${WORKDIR}'
 mkdir -p "${WORKDIR}/iso/${OSREV}/${MACHINE}"
 
 for f in cdboot cdbr; do
     echo "Downloading ${f}..."
     curl -sSf -o "${WORKDIR}/iso/${OSREV}/${MACHINE}/${f}" "${URL}/${f}"
 done
-for f in BOOTIA32.EFI BOOTX64.EFI bsd.rd ; do
+for f in BOOTIA32.EFI BOOTX64.EFI ; do
     echo "Downloading ${f}..."
     curl -sSf -o "${WORKDIR}/${f}" "${URL}/${f}"
 done
 
-_vnd="$(vnconfig -l | sed -n "s/^\(vnd[0-9]\): not in use/\1/p" | head -n 1)"
-if [ -z "$_vnd" ]; then
-    echo "ERROR: Cannot find free vnd device. Exiting..." 1>&2
-    exit 1
-fi
-
-echo "Creating custom bsd.rd..."
-zcat "${WORKDIR}/bsd.rd" > "${WORKDIR}/bsd.urd"
-rdsetroot -x "${WORKDIR}/bsd.urd" "${WORKDIR}/bsd.fs"
-vnconfig "$_vnd" "${WORKDIR}/bsd.fs"
-mount "/dev/${_vnd}a" "${WORKDIR}/mount"
-install -m 0644 auto_install.conf "${WORKDIR}/mount/auto_install.conf"
-install -m 0644 autopart.conf "${WORKDIR}/mount/autopart.conf"
-umount "${WORKDIR}/mount"
-rdsetroot "${WORKDIR}/bsd.urd" "${WORKDIR}/bsd.fs"
-compress -9 < "${WORKDIR}/bsd.urd" > "${WORKDIR}/iso/${OSREV}/${MACHINE}/bsd.rd"
+./create-rd.sh "$URL"
 
 echo "Create boot.conf..."
 mkdir "${WORKDIR}/iso/etc"
@@ -67,7 +43,7 @@ makefs -t msdos -o create_size=350K \
     "${WORKDIR}/iso/${OSREV}/${MACHINE}/eficdboot" "${WORKDIR}/eficdboot-dir"
 
 echo "Creating iso image..."
-mkhybrid -a -R -T -L -l -d -D -N -o "auto$(echo "$OSREV" | tr -d '.').iso" \
+mkhybrid -a -R -T -L -l -d -D -N -o "${MACHINE}/auto$(echo "$OSREV" | tr -d '.').iso" \
     -A "OpenBSD ${OSREV} ${MACHINE} Autoinstall CD" \
     -P "Copyright (c) $(date +%Y) The OpenBSD project" \
     -p "Timo Maekinen <tmakinen@foo.sh>" \
---
+++ create-rd.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -eu
+umask 022
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $(basename "$0") <URL>" 1>&2
+    exit 1
+fi
+
+URL="$1"
+MACHINE="$(basename "$1")"
+OSREV="$(basename "$(dirname "$1")")"
+
+WORKDIR="$(mktemp -d)"
+trap '{
+    if [ -n "$_vnd" ]; then
+        mount | grep -Eq "^/dev/${_vnd}a on ${WORKDIR}/mount " && \
+            umount "${WORKDIR}/mount"
+        vnconfig -l "$_vnd" | grep -Fq "not in use" || \
+            vnconfig -u "$_vnd"
+    fi
+    rm -rf "${WORKDIR}"
+}' EXIT
+mkdir "${WORKDIR}/mount"
+
+echo "Downloading bsd.rd..."
+curl -sSf -o "${WORKDIR}/bsd.rd" "${URL}/bsd.rd"
+
+_vnd="$(vnconfig -l | sed -n "s/^\(vnd[0-9]\): not in use/\1/p" | head -n 1)"
+if [ -z "$_vnd" ]; then
+    echo "ERROR: Cannot find free vnd device. Exiting..." 1>&2
+    exit 1
+fi
+
+echo "Creating custom bsd.rd..."
+
+if file -b "${WORKDIR}/bsd.rd" | grep -qE "^gzip compressed data, " ; then
+    compress=true
+    zcat "${WORKDIR}/bsd.rd" > "${WORKDIR}/bsd.urd"
+else
+    compress=false
+    mv "${WORKDIR}/bsd.rd" "${WORKDIR}/bsd.urd"
+fi
+rdsetroot -x "${WORKDIR}/bsd.urd" "${WORKDIR}/bsd.fs"
+vnconfig "$_vnd" "${WORKDIR}/bsd.fs"
+mount "/dev/${_vnd}a" "${WORKDIR}/mount"
+install -m 0644 auto_install.conf "${WORKDIR}/mount/auto_install.conf"
+install -m 0644 autopart.conf "${WORKDIR}/mount/autopart.conf"
+umount "${WORKDIR}/mount"
+rdsetroot "${WORKDIR}/bsd.urd" "${WORKDIR}/bsd.fs"
+
+mkdir "${MACHINE}"
+if [ "$compress" ]; then
+    compress -9 < "${WORKDIR}/bsd.urd" > "${MACHINE}/bsd.rd"
+else
+    cat "${WORKDIR}/bsd.urd" > "${MACHINE}/bsd.rd"
+fi
